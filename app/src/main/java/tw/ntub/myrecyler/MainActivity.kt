6e6e3cf0665e89c9dc5931b62077121aac04f05c
leonjye <leonjye@ntub.edu.tw>
2024-05-01 14:48:51 +0800

    add ViewHolder

--- app/src/main/java/tw/ntub/myrecyler/MainActivity.kt
+++ app/src/main/java/tw/ntub/myrecyler/MainActivity.kt
@@ -1,11 +1,40 @@
 package tw.ntub.myrecyler
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-data class Contact(val name:String, val phone: String)
+import androidx.recyclerview.widget.RecyclerView
+
+data class Contact(
+    val name:String,
+    val phone: String
+)
+
+class MyAdapter(private val data: ArrayList<Contact>): RecyclerView.Adapter<MyAdapter.ViewHolder>(){
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+        val imageView = view.findViewById<ImageView>(R.id.imageView)
+        val txtName = view.findViewById<TextView>(R.id.txtName)
+        val txtPhone = view.findViewById<TextView>(R.id.txtPhone)
+        val imageButton = view.findViewById<ImageButton>(R.id.imageButton)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+    override fun getItemCount() = data.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
