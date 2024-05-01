f41b91cd98a6ce90a7a0cd204b024c9001ca8b1c
leonjye <leonjye@ntub.edu.tw>
2024-05-01 14:26:39 +0800

    專案建立

---
+++ app/src/main/java/tw/ntub/myrecyler/MainActivity.kt
@@ -0,0 +1,20 @@
+package tw.ntub.myrecyler
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+data class Contact(val name:String, val phone: String)
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
